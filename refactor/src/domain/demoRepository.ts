2d68127a81d8b243f957f4aacc60850570d40357
TheMaJunjian <mx37163@163.com>
2026-08-01 10:27:21 +0800

    Revert "有趣的重构，看起来像报纸，这个风格可以，我很喜欢"
    This reverts commit fa5602f72ef00c71346d7dcab974f150eade6d9f.

--- refactor/src/domain/demoRepository.ts
+++
@@ -1,4 +0,0 @@
-import { fixtureSnapshot } from './fixtures';
-import { InMemoryTopicRepository } from './topicSnapshot';
-
-export const topicRepository = new InMemoryTopicRepository([fixtureSnapshot]);
